d4106be4e770c32e664b0322247150206bbca65d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2020-11-27 18:35:36 +0300

    mvp page

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { AppBar, Grid, Hidden, MenuItem, Toolbar } from '@material-ui/core';
+import { AppBar, Grid, MenuItem, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
-import ComingSoon from 'components/ComingSoon';
-import IconLogo from 'components/IconLogo';
 import Link from 'components/Link';
-import MenuBar from 'components/Header/MenuBar';
-import SocialMedia from 'components/SocialMedia';
 
 const useStyles = makeStyles((theme) => ({
+  navBarText: {
+    color: '#2FB56B',
+    transition: 'all .5s ease-in-out',
+    '&:hover': {
+      transform: 'scale(1.1)',
+      color: '#f3f33',
+    },
+    fontFamily: 'Anton',
+  },
+  navLink: {
+    textDecoration: 'none',
+    color: '#2FB56B',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+  toolbar: {
+    [theme.breakpoints.up('md')]: {
+      paddingRight: '8%',
+      paddingLeft: '8%',
+    },
+  },
   root: {
     flexGrow: 1,
     maxWidth: '100%',
@@ -18,103 +35,20 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#2e2e2e',
     boxShadow: 'none',
   },
-  toolbar: {
-    [theme.breakpoints.up('md')]: {
-      paddingRight: '8%',
-      paddingLeft: '8%',
-    },
+  navBarRoot: {
+    display: 'flex',
+    padding: '20px 0',
   },
-  airText: {
-    color: '#2FB56B',
-    transition: 'all .5s ease-in-out',
-    '&:hover': {
-      transform: 'scale(1.1)',
-      color: '#f3f33',
-    },
+  titleContainer: {
+    display: 'flex',
     fontFamily: 'Anton',
-  },
-  waterText: {
-    color: '#4972B8',
     transition: 'all .5s ease-in-out',
-    '&:hover': {
-      transform: 'scale(1.1)',
-      color: '#f3f33',
-    },
-    fontFamily: 'Anton',
-  },
-  soundText: {
-    color: '#B64598',
-    transition: 'all .5s ease-in-out',
-    '&:hover': {
-      transform: 'scale(1.1)',
-      color: '#f3f33',
-    },
-    fontFamily: 'Anton',
-  },
-  radiationText: {
-    color: '#F57C00',
-    transition: 'all .5s ease-in-out',
-    '&:hover': {
-      transform: 'scale(1.1)',
-      color: '#f3f33',
-    },
-    fontFamily: 'Anton',
-  },
-  // searchFa: {
-  //   padding: theme.spacing(1.5),
-  //   color: '#454545'
-  // },
-  airlink: {
-    textDecoration: 'none',
-    color: '#2FB56B',
-    '&:hover': {
-      textDecoration: 'none',
-    },
-  },
-  waterlink: {
-    textDecoration: 'none',
-    color: '#4972B8',
-  },
-  soundlink: {
-    textDecoration: 'none',
-    color: '#B64598',
-  },
-  radiationlink: {
-    textDecoration: 'none',
-    color: '#F57C00',
-  },
-  logoGrid: {
-    paddingTop: '0.4rem',
-  },
-  mediaGrid: {
-    paddingRight: theme.spacing(1),
+    paddingTop: '20px',
   },
 }));
 
 function Navbar(props) {
   const classes = useStyles(props);
-  const [show, setShow] = useState(false);
-  const [location, setLocation] = useState('/');
-
-  const showComingSoonAlert = (e, pathname) => {
-    if (e) {
-      e.preventDefault();
-    }
-    if (pathname) {
-      setLocation(window.location.pathname);
-      window.history.pushState(null, '', pathname);
-    }
-    setShow(true);
-  };
-  const showWaterComingSoonAlert = (e) => showComingSoonAlert(e, '/water');
-  const showSoundComingSoonAlert = (e) => showComingSoonAlert(e, '/sound');
-  const showRadiationComingSoonAlert = (e) =>
-    showComingSoonAlert(e, '/radiation');
-
-  const hideComingSoonAlert = () => {
-    window.history.pushState(null, '', location);
-    setShow(false);
-  };
 
   return (
     <Grid
@@ -127,70 +61,49 @@ function Navbar(props) {
         {/* Position sticky is not universally supported so the attribute reverts to static when unavailable */}
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar className={classes.toolbar} disableGutters>
-            <div item className={classes.logoGrid}>
-              <IconLogo />
-            </div>
-            <Grid
-              container
-              direction="row"
-              justify="flex-start"
-              alignItems="center"
-              className={classes.linkGrid}
-            >
-              <MenuItem className={classes.airText}>
-                <Link href="/air" className={classes.airlink}>
-                  AIR
-                </Link>
-              </MenuItem>
-
-              <MenuItem className={classes.waterText}>
-                <a
-                  href="/water"
-                  className={classes.waterlink}
-                  onClick={showWaterComingSoonAlert}
-                >
-                  WATER
-                </a>
-              </MenuItem>
-              <MenuItem className={classes.soundText}>
-                <a
-                  href="/sound"
-                  className={classes.soundlink}
-                  onClick={showSoundComingSoonAlert}
-                >
-                  SOUND
-                </a>
-              </MenuItem>
-              <MenuItem className={classes.radiationText}>
-                <a
-                  href="/radiation"
-                  className={classes.radiationlink}
-                  onClick={showRadiationComingSoonAlert}
-                >
-                  RADIATION
-                </a>
-              </MenuItem>
-            </Grid>
-
-            <Hidden smDown>
+            <Grid container>
               <Grid
-                container
-                xs={4}
-                direction="row"
-                justify="flex-end"
-                alignItems="center"
-                className={classes.mediaGrid}
+                item
+                lg={12}
+                justify="center"
+                classes={{ root: classes.titleContainer }}
               >
-                <Grid item>
-                  <SocialMedia color="#2FB56B" />
-                </Grid>
+                Sensors Dashboard
               </Grid>
-            </Hidden>
-            <Grid item>
-              <MenuBar />
+              <Grid
+                item
+                lg={12}
+                justify="center"
+                classes={{ root: classes.navBarRoot }}
+              >
+                <MenuItem className={classes.navBarText}>
+                  <Link href="/" className={classes.navLink}>
+                    HOME
+                  </Link>
+                </MenuItem>
+                <MenuItem className={classes.navBarText}>
+                  <Link href="#map" className={classes.navLink}>
+                    MAP
+                  </Link>
+                </MenuItem>
+                <MenuItem className={classes.navBarText}>
+                  <Link href="#resources" className={classes.navLink}>
+                    RESOURCES
+                  </Link>
+                </MenuItem>
+                <MenuItem className={classes.navBarText}>
+                  <Link href="#partners" className={classes.navLink}>
+                    PARTNERS
+                  </Link>
+                </MenuItem>
+                <MenuItem className={classes.navBarText}>
+                  <Link href="#contacts" className={classes.navLink}>
+                    CONTACT
+                  </Link>
+                </MenuItem>
+              </Grid>
             </Grid>
           </Toolbar>
-          <ComingSoon show={show} onClose={hideComingSoonAlert} />
         </AppBar>
       </Grid>
     </Grid>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,51 @@
 import React from 'react';
+import Navbar from 'components/Header/Navbar';
+import PartnerLogos from 'components/PartnerLogos';
 
-import DocumentHead from 'components/DocumentHead';
-import { URLS } from 'components/DocumentHead/PageHeads';
-import Footer from 'components/Footer';
-import Hero from 'components/Landing/Hero';
+import { Grid } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  section: {
+    width: '100%',
+    height: '400px',
+    backgroundColor: 'black',
+    color: 'white',
+    textAlign: 'center',
+    margin: '50px 50px 50px 50px',
+    scrollMarginTop: '200px',
+  },
+  topMargin: {
+    marginTop: '200px',
+  },
+}));
 
 function Home() {
+  const classes = useStyles();
+
   return (
     <>
-      <DocumentHead url={URLS.HOME} />
-      <Hero />
-      <Footer />
+      <Navbar />
+      {/* <AboutHeader/> */}
+      {/* GERTRUDE: Temporary placement of what should be components */}
+      <Grid container>
+        <Grid
+          item
+          lg={12}
+          id="map"
+          className={`${classes.section} ${classes.topMargin}`}
+        >
+          Map
+        </Grid>
+        <Grid item lg={12} id="resources" className={classes.section}>
+          Resources
+        </Grid>
+        <Grid item lg={12} id="contacts" className={classes.section}>
+          Contacts
+        </Grid>
+      </Grid>
+      <PartnerLogos />
     </>
   );
 }
