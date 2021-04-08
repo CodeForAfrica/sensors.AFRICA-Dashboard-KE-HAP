3680fea263dbedfef55c4ba339ae1221de58a178
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-04-08 15:27:43 +0300

    change base url

--- public/dashboard/js/events.js
+++ public/dashboard/js/events.js
@@ -1,5 +1,5 @@
 const zoomMap = (value) => {
-  const baseUrl = 'https://map.data4sdgs.sensors.africa#';
+  const baseUrl = '/map/index.html/#';
   const newUrl = `${baseUrl}${countiesLocation[value].zoom}/${countiesLocation[value].latitude}/${countiesLocation[value].longitude}`;
 
   document.getElementById('map-iframe').src = newUrl;
