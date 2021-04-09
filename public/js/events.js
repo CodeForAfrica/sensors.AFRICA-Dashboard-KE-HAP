5182c3b7cdb6a08ffaa07dd8837343e7d516d69c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-04-09 22:24:27 +0300

    refactor

--- public/js/events.js
+++ public/js/events.js
@@ -2,7 +2,7 @@ const zoomMap = (value) => {
   const baseUrl = '/map/index.html/#';
   const newUrl = `${baseUrl}${countiesLocation[value].zoom}/${countiesLocation[value].latitude}/${countiesLocation[value].longitude}`;
 
-  const countyName = value[0].toUpperCase() + value.substring(1);
+  const countyName = countiesLocation[value].name;
 
   document.getElementById('map-iframe').src = newUrl;
   document.getElementById('county-name').innerHTML = countyName;
