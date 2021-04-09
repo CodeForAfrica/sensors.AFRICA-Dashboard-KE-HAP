86cfbd347c0f07fc2f676d71e22f7785a6ef586c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-04-09 18:01:30 +0300

    change county name

--- public/js/events.js
+++ public/js/events.js
@@ -2,5 +2,8 @@ const zoomMap = (value) => {
   const baseUrl = '/map/index.html/#';
   const newUrl = `${baseUrl}${countiesLocation[value].zoom}/${countiesLocation[value].latitude}/${countiesLocation[value].longitude}`;
 
+  const countyName = value[0].toUpperCase() + value.substring(1);
+
   document.getElementById('map-iframe').src = newUrl;
+  document.getElementById('county-name').innerHTML = countyName;
 };
