15e1fc2e7e87284cf4c649027fa53328bee18996
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-06-21 06:50:20 +0300

    date calculation

--- public/js/counties.js
+++ public/js/counties.js
@@ -491,7 +491,7 @@ window.onload = async () => {
   });
 
   // fetch data. Blocking JS? + quite slow
-  const data = await fetch('/api/data');
+  const data = await fetch('/api/data/?length=7');
   results = await data.json();
 
   countyGraphChange('Nairobi');
--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,9 +1,18 @@
 import { fetchAllNodes } from 'api';
 
 export default async function handler(req, res) {
+  const daysAgo = req.query.length;
+  const date = new Date();
+  const dateRange = date.setDate(date.getDate() - daysAgo);
+  let ISOFormat;
+
+  if (dateRange) {
+    ISOFormat = new Date(dateRange).toISOString();
+  }
+
   try {
     const data = await fetchAllNodes(
-      'https://api.sensors.africa/v1/node?last_notify__gte=2021-06-13T10:00'
+      `https://api.sensors.africa/v1/node?last_notify__gte=${ISOFormat}`
     );
     return res.status(200).json(data);
   } catch (err) {
