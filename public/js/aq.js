70d6066aa8f90b1c248dd8c17b5f56db72fa4664
_ Kilemensi <clemence@codeforafrica.org>
2021-06-22 16:20:54 +0300

    Handle /nodes results

---
+++ public/js/aq.js
@@ -0,0 +1,26 @@
+// NOTE: requires('db');
+
+async function loadNodes() {
+  let nodes = window.db.getItem('nodes');
+  if (!nodes) {
+    const results = await fetch('/api/nodes/?days=7');
+    const data = await results.json();
+    nodes = data.results;
+    window.db.setItem('nodes', nodes);
+  }
+  return nodes;
+}
+
+async function load() {
+  loadNodes();
+}
+
+async function getNodes() {
+  return loadNodes();
+}
+
+window.aq = {
+  load,
+  getNodes,
+  charts: {},
+};
