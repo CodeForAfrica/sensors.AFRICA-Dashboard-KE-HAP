d56f72c6d0598cd2a06fca05d779023acb537fa5
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-06-22 09:23:49 +0300

    eslint fixes

--- public/js/countyGraph.js
+++ public/js/countyGraph.js
@@ -33,7 +33,7 @@ const countyGraphChange = async (county) => {
   // Array of nodes in counties
   const countiesDataArr = [...Object.values(countiesMap), ...newArr];
 
-  new Chart(graph, {
+  const countyGraphChart = new Chart(graph, {
     type: 'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
     data: {
       labels: countiesArr,
@@ -102,8 +102,8 @@ const countyGraphChange = async (county) => {
       animation: {
         duration: 500,
         easing: 'easeOutQuart',
-        onComplete: function () {
-          var ctx = this.chart.ctx;
+        onComplete() {
+          const { ctx } = this.chart;
           ctx.font = Chart.helpers.fontString(
             Chart.defaults.global.defaultFontFamily,
             'normal',
@@ -111,21 +111,20 @@ const countyGraphChange = async (county) => {
           );
           ctx.textAlign = 'center';
           ctx.textBaseline = 'bottom';
-          this.data.datasets.forEach(function (dataset) {
-            for (var i = 0; i < dataset.data.length; i++) {
-              var model =
-                  dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-                total = dataset._meta[Object.keys(dataset._meta)[0]].total,
-                mid_radius =
-                  model.innerRadius +
-                  (model.outerRadius - model.innerRadius) / 2,
-                start_angle = model.startAngle,
-                end_angle = model.endAngle,
-                mid_angle = start_angle + (end_angle - start_angle) / 2;
-              var x = mid_radius * Math.cos(mid_angle);
-              var y = mid_radius * Math.sin(mid_angle);
+          this.data.datasets.forEach((dataset) => {
+            for (let i = 0; i < dataset.data.length; i += 1) {
+              const model =
+                dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model;
+              const total = dataset._meta[Object.keys(dataset._meta)[0]].total;
+              const midRadius =
+                model.innerRadius + (model.outerRadius - model.innerRadius) / 2;
+              const { startAngle } = model;
+              const { endAngle } = model;
+              const midAngle = startAngle + (endAngle - startAngle) / 2;
+              const x = midRadius * Math.cos(midAngle);
+              const y = midRadius * Math.sin(midAngle);
               ctx.fillStyle = '#fff';
-              var percent =
+              let percent =
                 String(Math.round((dataset.data[i] / total) * 100)) + '%';
               ctx.fillText(dataset.data[i], model.x + x, model.y + y);
               ctx.fillText(percent, model.x + x, model.y + y + 15);
