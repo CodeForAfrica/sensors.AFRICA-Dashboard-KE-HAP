615444eecaa17c6b2c5fb52804220b490bf8454b
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-06-29 13:59:06 +0300

    get range

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -7,8 +7,54 @@ const getAnalytics = () => {
   };
 
   const getValue = (sensorReading) => {
+    const { pm1, pm2, pm0, noise } = rangeMap;
     sensorReading.forEach((reading) => {
-      reading.sensordatavalues.forEach((data) => {});
+      reading.sensordatavalues.forEach((data) => {
+        switch (data.value_type) {
+          case 'P1':
+            pm1.high =
+              Number(data.value) > pm1.high ? Number(data.value) : pm1.high; // Extract to helper function. Repetitively ugly
+            if (pm1.low === 0) {
+              pm1.low = Number(data.value);
+            } else {
+              pm1.low =
+                Number(data.value) < pm1.low ? Number(data.value) : pm1.low;
+            }
+            break;
+          case 'P2':
+            pm2.high =
+              Number(data.value) > pm2.high ? Number(data.value) : pm2.high;
+            if (pm2.low === 0) {
+              pm2.low = Number(data.value);
+            } else {
+              pm2.low =
+                Number(data.value) < pm2.low ? Number(data.value) : pm2.low;
+            }
+            break;
+          case 'P0':
+            pm0.high =
+              Number(data.value) > pm0.high ? Number(data.value) : pm0.high;
+            if (pm0.low === 0) {
+              pm0.low = Number(data.value);
+            } else {
+              pm0.low =
+                Number(data.value) < pm0.low ? Number(data.value) : pm0.low;
+            }
+            break;
+          case 'noise_Leq':
+            noise.high =
+              Number(data.value) > noise.high ? Number(data.value) : noise.high;
+            if (noise.low === 0) {
+              noise.low = Number(data.value);
+            } else {
+              noise.low =
+                Number(data.value) < noise.low ? Number(data.value) : noise.low;
+            }
+            break;
+          default:
+            break;
+        }
+      });
     });
   };
   const nodes = window.db.getItem('nodes');
@@ -21,6 +67,8 @@ const getAnalytics = () => {
   sensorData.forEach((data) => {
     data.forEach((result) => getValue(result));
   });
+
+  console.log('OLOOO', rangeMap);
 };
 
 window.analytics = {
