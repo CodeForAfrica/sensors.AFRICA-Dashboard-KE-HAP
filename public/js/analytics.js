5d49c71a04de6cf6129ac353444b76b434fe7d58
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-06-29 09:32:23 +0300

    messy approach

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,6 +1,26 @@
 const getAnalytics = () => {
+  const rangeMap = {
+    pm1: { high: 0, low: 0 },
+    pm2: { high: 0, low: 0 },
+    pm0: { high: 0, low: 0 },
+    noise: { high: 0, low: 0 },
+  };
+
+  const getValue = (sensorReading) => {
+    sensorReading.forEach((reading) => {
+      reading.sensordatavalues.forEach((data) => {});
+    });
+  };
   const nodes = window.db.getItem('nodes');
-  console.log('TESTING ANALYTICS', nodes);
+
+  // Loop
+  const sensorData = nodes
+    .map((node) => node.sensors)
+    .map((data) => data.map((result) => result.sensordatas));
+
+  sensorData.forEach((data) => {
+    data.forEach((result) => getValue(result));
+  });
 };
 
 window.analytics = {
