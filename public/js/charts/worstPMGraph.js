92ba794c09eec7b454af79bf9021a0460fba608a
nerdlet <nyokabi@codeforafrica.org>
2021-07-15 14:08:54 +0300

    Remove console.log

--- public/js/charts/worstPMGraph.js
+++ public/js/charts/worstPMGraph.js
@@ -122,7 +122,6 @@ const PMTopFiveChart = async (type) => {
 
   // Highest 5 nodes of pm type
   const topFiveNodes = typeSorted.splice(0, 5);
-  console.log(topFiveNodes);
 
   const labels = topFiveNodes.map((item) => item.id);
   let data = Object.values(topFiveNodes).map((worstNodes) => worstNodes[type]);
